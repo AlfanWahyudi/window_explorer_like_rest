7a1ba1617cb306b7e232f0080ad8abe48c9cb6c5
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-08-14 06:49:03 +0700

    UPD: db schema and seed

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,8 +1,8 @@
 DROP DATABASE "window-explorer";
 
 CREATE DATABASE "window-explorer";
-
-CREATE TABLE IF NOT EXISTS folders
+DROP TABLE IF EXISTS folders;
+CREATE TABLE  folders
 (
   id serial,
   name varchar(255) not null,
@@ -11,25 +11,29 @@ CREATE TABLE IF NOT EXISTS folders
   primary key (id)
 );
 
-CREATE TABLE IF NOT EXISTS parent_folders
+DROP TABLE IF EXISTS parent_folders;
+CREATE TABLE  parent_folders
 (
   id int references folders(id) on delete cascade,
   as_root boolean not null default false,
   primary key (id)
 );
 
-CREATE TABLE IF NOT EXISTS child_folders
+DROP TABLE IF EXISTS child_folders;
+CREATE TABLE  child_folders
 (
   id int references folders(id) on delete cascade,
   parent_folder_id int references parent_folders (id) on delete cascade,
   primary key (id)
 );
 
-CREATE TABLE IF NOT EXISTS files
+DROP TABLE IF EXISTS files;
+CREATE TABLE files
 (
   id serial,
   folder_id int references folders (id) on delete cascade,
-  name varchar(255) not null,
+  name text not null,
+  path text not null,
   extension varchar(15) not null,
   size_kilobyte bigint not null,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -37,3 +41,18 @@ CREATE TABLE IF NOT EXISTS files
   primary key (id)
 );
 
+DROP TABLE IF EXISTS folder_and_file;
+CREATE TABLE  folder_and_file 
+(
+  id int not null,
+  name text,
+  path text,
+  extension varchar(15),
+  size_kilobyte bigint,
+  created_at TIMESTAMP not null,
+  updated_at TIMESTAMP not null,
+  source_table text
+);
+CREATE INDEX idx_folder_and_file_id ON folder_and_file (id);
+CREATE INDEX idx_folder_and_file_created_at ON folder_and_file (created_at);
+CREATE INDEX idx_folder_and_file_updated_at ON folder_and_file (updated_at);
--- src/db/seed.sql
+++ src/db/seed.sql
@@ -10,34 +10,40 @@ DELETE FROM files;
 -- folders table ---
 ALTER SEQUENCE folders_id_seq RESTART WITH 1;
 
-INSERT INTO folders (name) VALUES ('Documents');
-INSERT INTO folders (name) VALUES ('Pictures');
-INSERT INTO folders (name) VALUES ('Music');
-INSERT INTO folders (name) VALUES ('Videos');
-INSERT INTO folders (name) VALUES ('School');
-INSERT INTO folders (name) VALUES ('Middle School');
-INSERT INTO folders (name) VALUES ('High School');
-INSERT INTO folders (name) VALUES ('Files');
-INSERT INTO folders (name) VALUES ('Holiday');
-INSERT INTO folders (name) VALUES ('Study');
-INSERT INTO folders (name) VALUES ('Jazz');
-INSERT INTO folders (name) VALUES ('Rock');
+INSERT INTO folders (name) VALUES ('Root Folder 1'); -- 1 
+INSERT INTO folders (name) VALUES ('Root Folder 2'); -- 2
+INSERT INTO folders (name) VALUES ('Root Folder 3'); -- 3
+INSERT INTO folders (name) VALUES ('Root Folder 4'); -- 4
+INSERT INTO folders (name) VALUES ('Folder (1.1)'); -- 5
+INSERT INTO folders (name) VALUES ('Folder (1.2)'); -- 6
+INSERT INTO folders (name) VALUES ('Folder (2.1)'); -- 7
+INSERT INTO folders (name) VALUES ('Folder (2.1.1)'); -- 8
+INSERT INTO folders (name) VALUES ('Folder (3.1)'); -- 9
+INSERT INTO folders (name) VALUES ('Folder (4.1)'); -- 10
+INSERT INTO folders (name) VALUES ('Folder (4.2)'); -- 11
+INSERT INTO folders (name) VALUES ('Folder (4.2.1)'); -- 12
 
+
+--- folder_and_file table ---
+INSERT INTO folder_and_file (id, name, created_at, updated_at, source_table)
+SELECT id, name, created_at, updated_at, 'folders_table'
+FROM folders
 
 -- parent_folders table ---
 INSERT INTO parent_folders (id, as_root) VALUES (1, true);
 INSERT INTO parent_folders (id, as_root) VALUES (2, true);
 INSERT INTO parent_folders (id, as_root) VALUES (3, true);
 INSERT INTO parent_folders (id, as_root) VALUES (4, true);
-INSERT INTO parent_folders (id, as_root) VALUES (5, true);
+INSERT INTO parent_folders (id, as_root) VALUES (7, false);
+INSERT INTO parent_folders (id, as_root) VALUES (11, true);
 
 
 -- child_folders table ---
 INSERT INTO child_folders (id, parent_folder_id) VALUES (5, 1);
-INSERT INTO child_folders (id, parent_folder_id) VALUES (6, 5);
-INSERT INTO child_folders (id, parent_folder_id) VALUES (7, 5);
-INSERT INTO child_folders (id, parent_folder_id) VALUES (8, 1);
-INSERT INTO child_folders (id, parent_folder_id) VALUES (9, 2);
-INSERT INTO child_folders (id, parent_folder_id) VALUES (10, 2);
-INSERT INTO child_folders (id, parent_folder_id) VALUES (11, 3);
-INSERT INTO child_folders (id, parent_folder_id) VALUES (12, 3);
+INSERT INTO child_folders (id, parent_folder_id) VALUES (6, 1);
+INSERT INTO child_folders (id, parent_folder_id) VALUES (7, 2);
+INSERT INTO child_folders (id, parent_folder_id) VALUES (8, 7);
+INSERT INTO child_folders (id, parent_folder_id) VALUES (9, 3);
+INSERT INTO child_folders (id, parent_folder_id) VALUES (10, 4);
+INSERT INTO child_folders (id, parent_folder_id) VALUES (11, 4);
+INSERT INTO child_folders (id, parent_folder_id) VALUES (12, 11);
